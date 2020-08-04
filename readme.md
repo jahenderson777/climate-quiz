5c80174fe85e699bc82a3cbfbb6750ddc3cca6c1
Jonathan Henderson <jonathan@Jonathans-MacBook-Pro.local>
2020-08-04 14:59:55 +0100

    styling

--- readme.md
+++ readme.md
@@ -1,3 +1,52 @@
 ## scratch 
 
-"framer-motion": "1.11.1",
+#_(defn animated []
+  [:> (.-div motion)
+   {:initial #js {:opacity 1}
+    :animate #js {:opacity 0.2}
+    :transition #js {:duration 2}
+    }
+   "Animated element2"])
+
+#_(defn use-animation-component []
+  (let [controls (useAnimation)]
+    (react/useEffect (fn []
+                       (.start controls (fn [i]
+                                          #js {;; :opacity 0
+                                               :x 100
+                                               :transition #js {:delay (* 0.3 i)}}))
+                       (fn []))
+                     #js [])
+    [:ul
+     (for [i (range 3)]
+       [:> (.-li motion)
+        {:key i
+         :custom i
+         :animate controls}
+        "Item " i])]))
+
+(comment
+  [:h1 "AnimatePresence223"]
+  [:> (.-div motion) {:style {:background-color "green"
+                              :display "block"
+                              :width 200}
+                      :transition #js {:duration 2}
+
+                      :animate #js {:scale 0.3}}
+   "FOOO"]
+  [:> (.-div motion) {:style {:color "red"
+                              :background-color "green"}
+
+                      :whileHover #js {:scale 2}}
+   "a link"]
+
+  (when (<sub [:get :foo])
+    [animated])
+  [:button
+   {:on-click #(xf/dispatch [:set :foo true])}
+   "Toggle"]
+
+  [:h1 "useAnimation"]
+  [use-animation-component])
+
+              ;[cljs-bean.core :as bean]
--- src/climate_quiz/core.cljs
+++ src/climate_quiz/core.cljs
@@ -3,13 +3,10 @@
             [uix.core.alpha :as uix]
             [xframe.core.alpha :as xf :refer [<sub]]
             [ajax.core :as ajax]
-            ;[cljs-bean.core :as bean]
             ["react" :as react]
             ["framer-motion" :refer [motion AnimatePresence useAnimation]]
             ["smoothscroll-polyfill" :as smooth]
             ))
-
-
 
 (set! *warn-on-infer* true)
 
@@ -20,15 +17,7 @@
                                              #(xf/dispatch [on-failed %])
                                              println)
                             :keywords? true
-                            :response-format :json})
-              #_(-> (js/fetch url)
-                  (.then #(if (.-ok %)
-                            (.json %)
-                            (if on-failed
-                              (xf/dispatch [on-failed %])
-                              (println %))))
-                  (.then #(js->clj % :keywordize-keys true))
-                  (.then #(xf/dispatch [on-ok %])))))
+                            :response-format :json})))
 
 (xf/reg-fx :http-post
            (fn [_ [_ {:keys [url params]}]]
@@ -60,12 +49,7 @@
 (xf/reg-event-fx
  :select-answer
  (fn [db [_ q-id a-id]]
-   {:db (-> db
-            (update :current-question (fn [old-current]
-                                        (if (<= old-current (inc q-id))
-                                          (inc old-current)
-                                          old-current)))
-            (assoc-in [:quiz q-id :selected] a-id))
+   {:db (assoc-in db [:quiz q-id :selected] a-id)
     :http-post {:url "https://www.appezium.co.uk/quiz.php"
                 :params {:id (:id db)
                          :q-id q-id
@@ -75,6 +59,11 @@
  :get
  (fn [& ks]
    (get-in (xf/<- [::xf/db]) ks)))
+
+(xf/reg-sub
+ :current-question
+ (fn [_]
+   (inc (count (filter :selected (xf/<- [:get :quiz]))))))
 
 (xf/reg-sub
  :questions
@@ -107,94 +96,71 @@
  (fn [_]
    (count (xf/<- [:get :quiz]))))
 
-#_(defn animated []
-  [:> (.-div motion)
-   {:initial #js {:opacity 1}
-    :animate #js {:opacity 0.2}
-    :transition #js {:duration 2}
-    }
-   "Animated element2"])
+(defn hsl
+  ([hue sat lightness]
+   (hsl hue sat lightness 1))
+  ([hue sat lightness alpha]
+   (str "hsla(" hue ", " sat "%, " lightness "%, " alpha ")")))
 
-#_(defn use-animation-component []
-  (let [controls (useAnimation)]
-    (react/useEffect (fn []
-                       (.start controls (fn [i]
-                                          #js {;; :opacity 0
-                                               :x 100
-                                               :transition #js {:delay (* 0.3 i)}}))
-                       (fn []))
-                     #js [])
-    [:ul
-     (for [i (range 3)]
-       [:> (.-li motion)
-        {:key i
-         :custom i
-         :animate controls}
-        "Item " i])]))
+(defn gradient [direction & stops]
+  (str "linear-gradient(" direction "deg,"
+       (apply str (interpose ", "
+                             (for [[h s l a pct-pos] stops]
+                               (str (hsl h s l a) " " pct-pos "% "))))
+       ")"))
 
-(defn question [q-id]
-  (let [ref (uix/ref)
-        {:keys [question answers correct-index selected]} (<sub [:get :quiz q-id])]
+(defn answer-component [idx answer q-id selected]
+  [:> (.-div motion) {:class "answer"
+                      :initial #js {:opacity 0}
+                      :animate #js {:opacity 1}
+                      :transition #js {:duration 0.20
+                                       :delay (+ 0.35 (* idx 0.2))}
+                      :style {:background-color (when (= idx selected) "#0f0")}
+                      :on-click #(xf/dispatch [:select-answer q-id idx])}
+   [:> (.-div motion)
+    {:whileHover #js {:x 33}
+     :whileTap #js {:x 33}}
+    answer]])
+
+(defn slide [{:keys [hue dir]} & content]
+  (let [ref (uix/ref)]
     (uix/effect! (fn []
                    (js/window.scrollTo #js {:left 0
                                             :top (.. @ref -offsetTop)
                                             :behavior "smooth"}))
                  [])
-    [:div {:ref ref
-           :style {:min-height (second (<sub [:get :window-size]))}}
+    [:div.slide {:ref ref
+                 :style {:min-height (second (<sub [:get :window-size]))
+                         :background (gradient dir
+                                               [hue 10 90 1 0]
+                                               [hue 10 90 1 60]
+                                               [0 70 88 1 100])}}
+     [:div.slide-inner
+      content]]))
+
+(defn question [q-id]
+  (let [{:keys [question answers correct-index selected]} (<sub [:get :quiz q-id])]
+    [slide {:hue (* 90 q-id)
+            :dir (if (even? q-id) 90 270)}
+     [:div.progress {:style {:padding 20
+                             :text-align "center"}}
+      (str "Question " (inc q-id) " of " (<sub [:num-questions]))]
      [:h2 question]
      (map-indexed
       (fn [idx answer]
         ^{:key idx}
-        [:> (.-div motion) {:class "answer"
-                            :initial #js {:opacity 0}
-                            :animate #js {:opacity 1}
-                            :transition #js {:duration 0.20
-                                             :delay (+ 0.35 (* idx 0.2))}
-                            :style {:background-color (when (= idx selected) "#0f0")}
-                            :on-click #(xf/dispatch [:select-answer q-id idx])}
-         answer])
+        [answer-component idx answer q-id selected])
       answers)]))
 
-(comment
-  [:h1 "AnimatePresence223"]
-  [:> (.-div motion) {:style {:background-color "green"
-                              :display "block"
-                              :width 200}
-                      :transition #js {:duration 2}
-
-                      :animate #js {:scale 0.3}}
-   "FOOO"]
-  [:> (.-div motion) {:style {:color "red"
-                              :background-color "green"}
-
-                      :whileHover #js {:scale 2}}
-   "a link"]
-
-  (when (<sub [:get :foo])
-    [animated])
-  [:button
-   {:on-click #(xf/dispatch [:set :foo true])}
-   "Toggle"]
-
-  [:h1 "useAnimation"]
-  [use-animation-component])
-
 (defn complete-block []
-  (let [ref (uix/ref)
-        _ (uix/effect! #(js/window.scrollTo #js {:left 0
-                                                 :top (.. @ref -offsetTop)
-                                                 :behavior "smooth"})
-                       [])]
-    [:div {:ref ref
-           :style {:background-color "#8a8"
-                   :min-height (second (<sub [:get :window-size]))}}
-     [:div "quiz complete"]
-     [:div (str "you scored " (<sub [:num-correct]) " out of " (<sub [:num-questions]))]]))
+  [slide {:hue 300 :dir 180}
+   [:h1 "Quiz complete!"]
+   [:h2 (str "you scored " (<sub [:num-correct]) " out of " (<sub [:num-questions]))]])
 
 (defn main []
   [:div#content
-   (for [q-id (range (<sub [:get :current-question]))]
+   (for [q-id (range (min (<sub [:current-question])
+                          (<sub [:num-questions])))]
      ^{:key q-id}
      [question q-id])
    
