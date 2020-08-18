853248b4a79bbe45f62af34d7e450ea7cedf1713
Jonathan Henderson <jonathan@Jonathans-MacBook-Pro.local>
2020-08-18 13:42:37 +0100

    .

--- resources/public/analytics.php
+++ resources/public/analytics.php
@@ -5,9 +5,5 @@ $fp = fopen('data.txt', 'a');
 fwrite($fp, json_encode($_POST)."\n");
 fclose($fp);
 
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
-
 echo "ok";
 ?>
